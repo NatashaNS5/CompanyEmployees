309adcfbaef9a3f4948ca961bd0712f1eddc690f
Наталья Сидорова <nataha1654@gmail.com>
2025-04-11 23:39:40 +0300

    lab8

---
+++ ActionFilters/Filters/ActionFilterAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionFilters.Filters
+{
+    public abstract class ActionFilterAttribute : Attribute, IActionFilter, IFilterMetadata, IAsyncActionFilter, IResultFilter, IAsyncResultFilter, IOrderedFilter
+    {
+
+    }
+}
